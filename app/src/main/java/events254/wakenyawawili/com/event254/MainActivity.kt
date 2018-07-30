b7013928ad316cde095294bf855f6aecf0a184ae
barney <brnbsjm@gmail.com>
2018-07-30 14:44:34 +0300

    Add splash screen handler, setup new activity to handle intent

--- app/src/main/java/events254/wakenyawawili/com/event254/MainActivity.kt
+++ app/src/main/java/events254/wakenyawawili/com/event254/MainActivity.kt
@@ -1,12 +1,35 @@
 package events254.wakenyawawili.com.event254
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASHDELAY: Long = 3000;
+
+    private val mRunnable: Runnable = Runnable {
+        if (isFinishing) {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mDelayHandler = Handler(); // initialize handler
+        mDelayHandler!!.postDelayed(mRunnable, SPLASHDELAY)
+    }
+
+    public override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
     }
 }
